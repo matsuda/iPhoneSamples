2a13406bc739b4b57403daa685f27e4d360165bd
matsuda <hi@matsuda.me>
2011-12-05 14:52:29 +0900

    Carouselサンプルの追加

---
+++ TestCarousel/TestCarousel/TestCarouselViewController.h
@@ -0,0 +1,15 @@
+//
+//  TestCarouselViewController.h
+//  TestCarousel
+//
+//  Created by Kosuke Matsuda on 11/12/05.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestCarouselViewController : UIViewController
+
+@property (nonatomic, retain) UIScrollView *scrollView;
+
+@end
