13c88c2cbb2ab5675ce3a8c36596f21c7547b816
matsuda <hi@matsuda.me>
2011-12-12 10:15:27 +0900

    create project. vertical and horizontal scrolls

---
+++ TestMultiScroll/TestMultiScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestMultiScroll
+//
+//  Created by Kosuke Matsuda on 11/12/12.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
