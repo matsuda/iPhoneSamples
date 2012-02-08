ba2092554d538d30016bd954b3cb903d068f12f6
matsuda <hi@matsuda.me>
2012-02-08 12:00:51 +0900

    create repository for testing activerecord library

---
+++ TestActiveRecord/TestActiveRecord/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestActiveRecord
+//
+//  Created by matsuda on 12/02/08.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
