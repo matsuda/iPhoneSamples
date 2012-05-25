f3569f9c4642d98f09cb45bf0a14dcaa1b9c3ebe
matsuda <hi@matsuda.me>
2012-05-25 16:24:27 +0900

    create project to custom TableViewCell

---
+++ TestCustomTableViewCell/TestCustomTableViewCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCustomTableViewCell
+//
+//  Created by matsuda on 12/05/25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
