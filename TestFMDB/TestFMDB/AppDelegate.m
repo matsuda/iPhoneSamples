e9cf2438b0cf2577b808fca1c0d439532ffbf1f9
matsuda <hi@matsuda.me>
2012-05-28 19:22:47 +0900

    Databaseファイルのセットアップ

--- TestFMDB/TestFMDB/AppDelegate.m
+++ TestFMDB/TestFMDB/AppDelegate.m
@@ -7,13 +7,40 @@
 //
 
 #import "AppDelegate.h"
+#import "FMDatabase.h"
 
 @implementation AppDelegate
 
 @synthesize window = _window;
 
+static NSString * const databaseName = @"test.sqlite";
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // FMDB
+    // NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *dir = [paths objectAtIndex:0];
+    NSLog(@"dir >>> %@", dir);
+
+    NSString *applicationName = [[[NSBundle mainBundle] infoDictionary] valueForKey:(NSString *)kCFBundleNameKey];
+    NSLog(@"app >>> %@", applicationName);
+    NSString *path = [dir stringByAppendingPathComponent:applicationName];
+    NSLog(@"path >>> %@", path);
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    if (![fileManager fileExistsAtPath:path]) {
+        NSError *error = nil;
+        if ( ![fileManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:&error] ) {
+            NSLog(@"error > %@", error);
+        }
+    }
+    NSString *dbPath = [path stringByAppendingPathComponent:databaseName];
+    NSLog(@"dbPath >>> %@", dbPath);
+    FMDatabase *db = [FMDatabase databaseWithPath:dbPath];
+    [db open];
+    [db close];
+
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
