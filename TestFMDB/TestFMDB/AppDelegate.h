56b1452e38b7f6be6c769847637577b00a8fed0d
matsuda <hi@matsuda.me>
2012-05-28 18:14:28 +0900

    create project to test FMDB

---
+++ TestFMDB/TestFMDB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestFMDB
+//
+//  Created by matsuda on 12/05/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
