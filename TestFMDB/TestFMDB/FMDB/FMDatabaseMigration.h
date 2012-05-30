0a300eed0edf4302b11af07e2686b7c4990cd1f4
matsuda <hi@matsuda.me>
2012-05-30 15:33:25 +0900

    説明文を追加

--- TestFMDB/TestFMDB/FMDB/FMDatabaseMigration.h
+++ TestFMDB/TestFMDB/FMDB/FMDatabaseMigration.h
@@ -8,6 +8,33 @@
 
 #import "FMDatabase.h"
 
+/*
+ ////////////////////////////////////////////////////////////////
+
+ -- AppDelegate.m
+
+ * use databaseWithFileName method instead of databaseWithPath
+ * call migrateWithMigrator:migrator
+
+    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+    {
+        FMDatabase *database = [FMDatabase databaseWithFileName:@"test.sql"];
+        [database migrateWithMigrator:[[Migrator alloc] init]];
+        return YES;
+    }
+
+ -- Migrator.h
+
+ * describe "migrateVersion<version number>" method with an argument of FMDatabase object
+
+    #import "FMDatabaseMigration.h"
+    @interface Migrator : NSObject
+    - (void)migrateVersion1:(FMDatabase *)db;
+    @end
+
+ ////////////////////////////////////////////////////////////////
+ */
+
 typedef BOOL (^FMDBMigrationBlock)(FMDatabase *db, NSInteger version);
 
 @interface FMDatabase (FMDatabaseMigration)
