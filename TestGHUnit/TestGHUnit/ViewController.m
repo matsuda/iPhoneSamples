fed218d95f1db118167517285abbf9a8f2052560
matsuda <hi@matsuda.me>
2012-07-18 18:36:49 +0900

    create project and prepare to test with GHUnit.

---
+++ TestGHUnit/TestGHUnit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestGHUnit
+//
+//  Created by matsuda on 12/07/18.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TestGHUnit/Tests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tests
+//
+//  Created by matsuda on 12/07/18.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
