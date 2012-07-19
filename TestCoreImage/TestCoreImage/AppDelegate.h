11ddd9870dd190e58df7376123f516baa6e20576
matsuda <hi@matsuda.me>
2012-07-19 12:37:58 +0900

    create project to core image

---
+++ TestCoreImage/TestCoreImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCoreImage
+//
+//  Created by matsuda on 12/07/19.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestCoreImage/TestCoreImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestCoreImage
+//
+//  Created by matsuda on 12/07/19.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
