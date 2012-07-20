337893bb293cb8e75a2eab00764bfbead11b67f5
matsuda <hi@matsuda.me>
2012-07-20 11:03:59 +0900

    create project to pinch gesture

---
+++ TestPinch/TestPinch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestPinch
+//
+//  Created by matsuda on 12/07/20.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
