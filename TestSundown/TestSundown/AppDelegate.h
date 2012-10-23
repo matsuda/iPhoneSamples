6f41f736c6858c6fd9f20cebd10c6a353b788101
matsuda <hi@matsuda.me>
2012-10-23 16:02:35 +0900

    create project to test Markdown

---
+++ TestSundown/TestSundown/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSundown
+//
+//  Created by Kosuke Matsuda on 2012/10/23.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
