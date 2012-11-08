f0ba34a2ef0865850a48596bff5af18470754d43
matsuda <hi@matsuda.me>
2012-11-08 17:24:26 +0900

    アプリ起動時の傾きを考慮したview情報を取得するサンプル

---
+++ TestInterfaceOrientation/TestInterfaceOrientation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestInterfaceOrientation
+//
+//  Created by Kosuke Matsuda on 2012/11/08.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
