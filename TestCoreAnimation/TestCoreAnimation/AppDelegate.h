7dafa17c3ea41d9206d4ee797eccdd34c88cfc68
matsuda <hi@matsuda.me>
2012-11-15 12:03:16 +0900

    create projecto to test CoreAnimation

---
+++ TestCoreAnimation/TestCoreAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCoreAnimation
+//
+//  Created by Kosuke Matsuda on 2012/11/15.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestCoreAnimation/TestCoreAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestCoreAnimation
+//
+//  Created by Kosuke Matsuda on 2012/11/15.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
