76323e267f94941b4e7e2e97691e26d7a2f67310
matsuda <hi@matsuda.me>
2012-11-16 16:22:19 +0900

    CAKeyframeAnimationを試す

--- TestCoreAnimation/TestCoreAnimation/ViewController.m
+++ TestCoreAnimation/TestCoreAnimation/ViewController.m
@@ -42,13 +42,56 @@
 {
     NSLog(@"%s", __func__);
 //    [self fadeAnimation];
-    [self rotateAnimation];
+//    [self rotateAnimation];
+//    [self blinkAnimation];
+//    [self rollingAnimation];
+    [self translationAnimation];
+}
+
+- (void)translationAnimation
+{
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.duration = 0.2f;
+    animation.values = @[
+        [NSValue valueWithCATransform3D:CATransform3DIdentity],
+        [NSValue valueWithCATransform3D:CATransform3DMakeTranslation(100, 0, 0)],
+        [NSValue valueWithCATransform3D:CATransform3DMakeTranslation(80, 0, 0)]
+    ];
+    animation.keyTimes = @[[NSNumber numberWithFloat:0], [NSNumber numberWithFloat:0.45], [NSNumber numberWithFloat:1]];
+    animation.removedOnCompletion = NO;
+    animation.fillMode = kCAFillModeForwards;
+    [self.layer addAnimation:animation forKey:@"translation"];
+}
+
+- (void)rollingAnimation
+{
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.duration = 1.0;
+//    animation.repeatCount = FLT_MAX;
+    animation.values = @[
+        [NSValue valueWithCATransform3D:CATransform3DIdentity],
+        [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI * 90 / 180.f, 0, 0, 1.f)],
+        [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI * 180 / 180.f, 0, 0, 1.f)],
+        [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI * 270 / 180.f, 0, 0, 1.f)],
+        [NSValue valueWithCATransform3D:CATransform3DIdentity]
+    ];
+    [self.layer addAnimation:animation forKey:@"rollingAnimation"];
+}
+
+- (void)blinkAnimation
+{
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"opacity"];
+    animation.duration = 2.0f;
+    animation.repeatCount = FLT_MAX;
+    animation.values = @[[NSNumber numberWithFloat:1.f], [NSNumber numberWithFloat:0.f], [NSNumber numberWithFloat:1.f]];
+    [self.layer addAnimation:animation forKey:@"blinkAnimation"];
 }
 
 - (void)rotateAnimation
 {
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    animation.duration = 1.f;
+    animation.duration = 0.2;
+    animation.repeatCount = FLT_MAX;
     animation.toValue = [NSNumber numberWithFloat:(M_PI / 180) * 90];
 //    animation.removedOnCompletion = NO;
 //    animation.fillMode = kCAFillModeForwards;
