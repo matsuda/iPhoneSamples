133595f5d0ea848a103f4d8a9d218b99df139a38
matsuda <hi@matsuda.me>
2012-12-25 15:00:04 +0900

    create project for Camera

---
+++ TestCamera/TestCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCamera
+//
+//  Created by Kosuke Matsuda on 2012/12/25.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
