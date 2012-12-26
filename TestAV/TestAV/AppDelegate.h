d79f9fd1333d07f01719a950565356c5932e4d0d
matsuda <hi@matsuda.me>
2012-12-26 16:40:44 +0900

    create project to test AVFoundation

---
+++ TestAV/TestAV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestAV
+//
+//  Created by Kosuke Matsuda on 2012/12/26.
+//  Copyright (c) 2012年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
